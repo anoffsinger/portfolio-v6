43ec0448ef0622d0ba3b572ff9ed34f4a73517bc
Adam Noffsinger <adam.noffsinger@gmail.com>
2024-02-19 21:39:34 -0800

    Added safari blur fix

---
+++ index.da2f44c1.js
@@ -0,0 +1,2 @@
+let o,t,a;o=/^((?!chrome|android).)*safari/i.test(navigator.userAgent),t=/iPhone|iPad|iPod|Android/i.test(navigator.userAgent),a="ontouchstart"in window||navigator.maxTouchPoints>0||navigator.msMaxTouchPoints>0,!o||t||a||document.querySelectorAll(".work-pill").forEach(function(o){o.classList.add("safari-desktop")});
+//# sourceMappingURL=index.da2f44c1.js.map
